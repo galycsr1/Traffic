1c42bd51c8c29df69cceb2af5615b17ff6f59556
avnerrhh <huriavne@post.bgu.ac.il>
2017-12-27 12:45:34 +0200

    reading from a meta file to json files

---
+++ server/read.py
@@ -0,0 +1,12 @@
+import os
+import json
+metadata = open('big_22.12.2017-10_28_44.meta','r')
+i = 0
+frame = metadata.readline()[:-1].replace("'", '"').replace("False", "false").replace("True", "true")
+while (frame) :
+    name = 'frames/f' + str(i) +'.json'
+    file = open(name, 'w')
+    frame = metadata.readline()[:-1].replace("'", '"').replace("False", "false").replace("True", "true")
+    file.write(frame)
+    file.close()
+    i+=1
