6ebf311e7b9e65b53152d91110ff1b98614d2998
avnerrhh <huriavne@post.bgu.ac.il>
2017-12-28 14:35:28 +0200

    a lot of new staff

--- server/read.py
+++ server/read.py
@@ -1,13 +1,38 @@
 import os
 import json
-#ח
-metadata = open('big_22.12.2017-10_28_44.meta','r')
-i = 0
-frame = metadata.readline()[:-1].replace("'", '"').replace("False", "false").replace("True", "true")
-while (frame) :
-    name = 'frames/f' + str(i) +'.json'
-    file = open(name, 'w')
+
+
+def ReadMetaFile(name):
+    metadata = open('metas/' + name, 'r')
+    i = 0
     frame = metadata.readline()[:-1].replace("'", '"').replace("False", "false").replace("True", "true")
-    file.write(frame)
-    file.close()
-    i+=1
+    while frame:
+        if not os.path.exists('frames/' + name[:-5]):
+            os.makedirs('frames/' + name[:-5])
+        file = open('frames/' + name[:-5] + '/f' + str(i) + '.json', 'w')
+        frame = metadata.readline()[:-1].replace("'", '"').replace("False", "false").replace("True", "true")
+        file.write(frame)
+        file.close()
+        i += 1
+
+
+def ReadMllMetas():
+    from os import listdir
+    from os.path import isfile, join
+    onlyfiles = [f for f in listdir("metas") if isfile(join("metas", f))]
+    for i in range(0, len(onlyfiles)):
+        ReadMetaFile(onlyfiles[i])
+
+def MakeVehiclesFromeFrames(path):
+
+
+
+
+
+def MakeCarsFromFrame():
+    from os import listdir
+    from os.path import isfile, join
+    paths = [f for f in listdir("frames") if isfile(join("frames", f))]
+    for i in range(0, len(paths)):
+        vehicles = MakeVehiclesFromeFrames(paths[i])
+
