55bbe2ba63ffd33c40e1ce5a683ea53a625aa32f
avnerrhh <huriavne@post.bgu.ac.il>
2017-12-28 13:53:35 +0200

    working

--- server/read.py
+++ server/read.py
@@ -19,12 +19,23 @@ import json
         file.close()
         i += 1
 '''
+
+def ReadMetaFile():
+    file = open("big_28.12.2017-04_55_56.meta",'r')
+    data = fixFile(file.read())
+    print(data)
+    #file.write(data)
+
+
 def fixFile(data):
     jsons = data.replace("'", '"').replace("False", "false").replace("True", "true").split("\n")
+    ans=[]
     for json in jsons:
-        json=changeJson(json)
-        json = fixFrame(json)
-    return jsons
+        if json:
+           json=changeJson(json)
+           json = fixFrame(json)
+           ans.append(json)
+    return ans
 
 
 '''def ReadAllMetas():
@@ -64,6 +75,7 @@ def getLane(x,y):
 
 
 def fixFrame(frame):
+    ans=[]
     for i in range (0,len(frame['objects'])):
         object = frame['objects'][i]
         x = object['bounding_box'][0]
@@ -77,8 +89,9 @@ def fixFrame(frame):
         if(y<0):
             y=0
         y *=2.666666
-        object['bounding_box'][0]=y
-    return frame
+        object['bounding_box'][1]=y
+        ans.append(object)
+    return ans
 
 
 def fixJson(jsonObject):
@@ -107,17 +120,19 @@ def fixJson(jsonObject):
         changeJsons(paths[i])
 
 '''
+import json
 
 
-
-def changeJson(jsFile):
+def changeJson(jsonFile):
     data = []
+    jsFile = json.loads(jsonFile)
     for objc in jsFile['objects']:
         updatedObj = {"type": objc['type'], "created_at": objc['created_at'], "tracking_id": objc['tracking_id'], "bounding_box": objc['bounding_box']}
         data.append(updatedObj)
     jsFile['objects'] = data
     return jsFile
 
+ReadMetaFile()
 
 #changeJson({"objects": [{"confidence": 0.92, "type": "bus", "static": True, "created_at": "2017-12-28 04:55:02.750086", "times_lost_by_convnet": 1, "speed": 13.65099956558118, "lost": False, "alert_tags": [], "tracking_id": 24488, "bounding_box": [386, 212, 105, 130], "new": False, "counted": False}, {"confidence": 0.58, "type": "car", "static": False, "created_at": "2017-12-28 04:55:41.927352", "times_lost_by_convnet": 1, "speed": 3.138550189376427, "lost": False, "alert_tags": [], "tracking_id": 24491, "bounding_box": [535, 304, 61, 57], "new": False, "counted": False}], "frame_index": 5443497})
 
