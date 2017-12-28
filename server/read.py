dad66b971a21a3d232ec5c9d79ad3bd8bb788921
avnerrhh <huriavne@post.bgu.ac.il>
2017-12-28 14:06:42 +0200

    a lot of changes

--- server/read.py
+++ server/read.py
@@ -2,7 +2,7 @@ import os
 import json
 
 
-def ReadMetaFile(name):
+'''def ReadMetaFile(name):
     metadata = open('metas/' + name, 'r')
     i = 0
     frame = metadata.readline()[:-1].replace("'", '"').replace("False", "false").replace("True", "true")
@@ -18,19 +18,23 @@ def ReadMetaFile(name):
         frame = metadata.readline()[:-1].replace("'", '"').replace("False", "false").replace("True", "true")
         file.close()
         i += 1
+'''
+def fixFile(data):
+    jsons = data.replace("'", '"').replace("False", "false").replace("True", "true").split("\n")
+    for json in jsons:
+        json=changeJson(json)
+        json = fixFrame(json)
+    return jsons
 
 
-def ReadMllMetas():
+'''def ReadAllMetas():
     from os import listdir
     from os.path import isfile, join
     onlyfiles = [f for f in listdir("metas") if isfile(join("metas", f))]
     for i in range(0, len(onlyfiles)):
-        print(onlyfiles[i])
+        #print(onlyfiles[i])
         ReadMetaFile(onlyfiles[i])
-
-
-def extractValidDataFromFrames():
-    return ""
+'''
 
 def getLane(x,y):
     if y>270:
@@ -60,7 +64,6 @@ def getLane(x,y):
 
 
 def fixFrame(frame):
-
     for i in range (0,len(frame['objects'])):
         object = frame['objects'][i]
         x = object['bounding_box'][0]
@@ -78,21 +81,32 @@ def fixFrame(frame):
     return frame
 
 
-
-
-
 def fixJson(jsonObject):
     for i  in range (0,len(jsonObject)):
         jsonObject[i] = fixFrame(0,jsonObject[i])
 
+'''def changeJsons(path):
+    slash = len(path)
+    for i in range (0, len(path)):
+        if path[i]=='/':
+            slash=1
+            break
+    from os import listdir
+    from os.path import isfile, join
+    onlyfiles = [f for f in listdir(path[slash:]) if isfile(path[slash:], f)]
+    for i in range(0, len(onlyfiles)):
+'''
 
-def MakeCarsFromFrame():
+
+
+'''def fixFrames():
     from os import listdir
     from os.path import isfile, join
     paths = [f for f in listdir("frames") if isfile(join("frames", f))]
     for i in range(0, len(paths)):
-        vehicles = MakeVehiclesFromeFrames(paths[i])
-       fixJson("")
+        changeJsons(paths[i])
+
+'''
 
 
 
@@ -102,9 +116,8 @@ def changeJson(jsFile):
         updatedObj = {"type": objc['type'], "created_at": objc['created_at'], "tracking_id": objc['tracking_id'], "bounding_box": objc['bounding_box']}
         data.append(updatedObj)
     jsFile['objects'] = data
-    #print(jsFile)
+    return jsFile
 
-#ReadMllMetas()
-#ReadMetaFile('text.txt')
-changeJson({"objects": [{"confidence": 0.92, "type": "bus", "static": True, "created_at": "2017-12-28 04:55:02.750086", "times_lost_by_convnet": 1, "speed": 13.65099956558118, "lost": False, "alert_tags": [], "tracking_id": 24488, "bounding_box": [386, 212, 105, 130], "new": False, "counted": False}, {"confidence": 0.58, "type": "car", "static": False, "created_at": "2017-12-28 04:55:41.927352", "times_lost_by_convnet": 1, "speed": 3.138550189376427, "lost": False, "alert_tags": [], "tracking_id": 24491, "bounding_box": [535, 304, 61, 57], "new": False, "counted": False}], "frame_index": 5443497})
+
+#changeJson({"objects": [{"confidence": 0.92, "type": "bus", "static": True, "created_at": "2017-12-28 04:55:02.750086", "times_lost_by_convnet": 1, "speed": 13.65099956558118, "lost": False, "alert_tags": [], "tracking_id": 24488, "bounding_box": [386, 212, 105, 130], "new": False, "counted": False}, {"confidence": 0.58, "type": "car", "static": False, "created_at": "2017-12-28 04:55:41.927352", "times_lost_by_convnet": 1, "speed": 3.138550189376427, "lost": False, "alert_tags": [], "tracking_id": 24491, "bounding_box": [535, 304, 61, 57], "new": False, "counted": False}], "frame_index": 5443497})
 
