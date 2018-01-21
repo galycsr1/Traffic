bbd679e2fd89e862fd4847d70144a8f992b37dd2
avnerrhh <huriavne@post.bgu.ac.il>
2018-01-21 15:21:55 +0200

    bla

--- server/kodefun-httpserver.py
+++ server/kodefun-httpserver.py
@@ -8,7 +8,7 @@ import Parser
 import read
 
 def getFrames(text_http):
-    return Parser.fix_file(text_http)
+    return Parser.fix_file(text_http,"C:\\Users\\Avner\\PycharmProjects\\traffic data\\examples\\big_28.12.2017-05_05_06.json")
 
 #Create custom HTTPRequestHandler class
 class KodeFunHTTPRequestHandler(BaseHTTPRequestHandler):
