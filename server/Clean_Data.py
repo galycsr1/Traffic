da43aae497d83e4758e686447242a7c71994f8bc
BguIdan <idanmosh@post.bgu.ac.il>
2018-03-11 20:59:39 +0200

    Finish with stages 1 and 2 of normalizing the data

--- server/Clean_Data.py
+++ server/Clean_Data.py
@@ -55,31 +55,74 @@ def clean(data):
             coordinates[0] = vehicle['bounding_box'][0]
             coordinates[1] = vehicle['bounding_box'][1]
             hash_vehicles[vehicle_id].append(coordinates)
-            # if (vehicle_id ==24488):
-            #     print (hash_vehicles[vehicle_id])
+
     #hash_vehicles = clean_routs(hash_vehicles)
     normalizeData(hash_vehicles)
     return jsons, hash_vehicles
+
 
 def normalizeData(vehiclesPath):
     for path in vehiclesPath:
         start_location = vehiclesPath[path][0]
         end_location = vehiclesPath[path][len(vehiclesPath[path])-1]
-        # We'll check that every location is in the range in x axis and in y axis
-        # between the start and end location and delete location that doesn't satisfies that
+
+        # First Stage:
+        #   We'll check that every location is in the range in x axis and in y axis
+        #   between the start and end location and delete location that doesn't satisfies that
         vehiclesPath[path] = [location for location in vehiclesPath[path] if checkInRange(start_location[0], end_location[0], location[0])]
         vehiclesPath[path] = [location for location in vehiclesPath[path] if checkInRange(start_location[1], end_location[1], location[1])]
 
-        #for location in path:
-            #x_loc = location[0]
-            #y_loc = location[1]
-            #if not (start_location[0] <= x_loc <= end_location[0] or end_location[0] <= x_loc <= start_location[0])
-            #somelist = [x for x in somelist if not determine(x)]
+        # Second Stage:
+        #   We'll check(and fix if needed) that the vehicle movement is linear. Which means that if in x/y axis we start
+        #   in high number and end in lower number, the numbers should be going down all the way or vice versa.
+        linearMovement(start_location, vehiclesPath[path])
 
 def checkInRange(start, end, currenLocation):
     # check x/y axis
     ans = True
     if not (start <= currenLocation <= end or end <= currenLocation <= start):
-        # anaomality detected
+        # anomaly detected
         ans = False
+    return ans
+
+def linearMovement(start, path):
+    index = 1
+    # check if the movement is from higher to lower numbers or vice versa
+    directionX = checkForDirection(start, path[index], 0)
+    directionY = checkForDirection(start, path[index], 1)
+    while index < len(path):
+        # if not the last location in path
+        if index != len(path) - 1:
+            if directionX == "unknown":
+                directionX = checkForDirection(path[index], path[index+1], 0)
+            if directionY == "unknown":
+                directionY = checkForDirection(path[index], path[index + 1], 1)
+            # if isOK = True, move to the next index else delete the location in index+1
+            isOkX = checkIfLinear(path[index], path[index+1], directionX, 0)
+            isOkY = checkIfLinear(path[index], path[index+1], directionY, 1)
+            if not isOkX or not isOkY:
+                del(path[index+1])
+            else:
+                index += 1
+        else:
+            index += 1
+
+def checkForDirection(locFrom, locTo, xORy):
+    direction = "unknown"
+    if locFrom[xORy] > locTo[xORy]:
+        direction = "down"
+    elif locFrom[xORy] < locTo[xORy]:
+        direction = "up"
+    # else there is no difference between the locations
+    return direction
+
+def checkIfLinear(locFrom, locTo, direction, xORy):
+    ans = True
+    if direction != "unknown":
+        if direction == "up":
+            if locFrom[xORy] > locTo[xORy]:
+                ans = False
+        else: # direction = down
+            if locTo[xORy] > locFrom[xORy]:
+                ans = False
     return ans
