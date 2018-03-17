56b28ef65c814600dc5c4cd82655b2cfa4275046
avnerrhh <avnerrhh@gmail.com>
2018-03-17 12:44:14 +0200

    .

--- server/read.py
+++ server/read.py
@@ -72,12 +72,9 @@ def read_meta_file():
 '''
 
 
-
-
-
 # if calibration.is_lane([[557, 160], [263,422], [381, 445], [565, 163]], 387, 212) or calibration.is_lane([[591,173],[477,461],[626,461 ],[608,180],[599,176]],387, 212) or calibration.is_lane([[568,166],[377,447],[478,456],[589,172]], 387, 212):
 #     print("works")
 # is_lane([[591,173],[477,461],[626,461 ],[608,180],[599,176]],387, 212)
 # is_lane([568,166],[377,447],[478,456],[589,172], 387, 212)
-#print (read_meta_file())
+print (read_meta_file())
 # change_json({"objects": [{"confidence": 0.92, "type": "bus", "static": True, "created_at": "2017-12-28 04:55:02.750086", "times_lost_by_convnet": 1, "speed": 13.65099956558118, "lost": False, "alert_tags": [], "tracking_id": 24488, "bounding_box": [386, 212, 105, 130], "new": False, "counted": False}, {"confidence": 0.58, "type": "car", "static": False, "created_at": "2017-12-28 04:55:41.927352", "times_lost_by_convnet": 1, "speed": 3.138550189376427, "lost": False, "alert_tags": [], "tracking_id": 24491, "bounding_box": [535, 304, 61, 57], "new": False, "counted": False}], "frame_index": 5443497})
