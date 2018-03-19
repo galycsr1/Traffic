e0d76c65397ec3ca87fc5f814466ed18c9568079
benzilonka <ben.zilonka@gmail.com>
2018-03-19 19:19:17 +0200

    prepare to merge

--- server/Vehicle.py
+++
@@ -1,7 +0,0 @@
-class Vehicle:
-
-    def __init__(self, tracking_id, type, bbox, speed):
-        self.tracking_id = tracking_id
-        self.type = type
-        self.speed = speed
-        self.bbox = bbox
